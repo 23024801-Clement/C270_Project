5af54792337cc0b49de81ee1f50381bfa670f9ab
23024801-Clement <23024801@myrp.edu.sg>
2025-02-08 21:05:07 +0800

    Delete test.py

--- test.py
+++
@@ -1,7 +0,0 @@
-import pytest
-from test import app
-def test_addition():
-    """Test case for addition operation"""
-    with app.test_client() as client: #creating client
-        response = client.post("/", data={"num1": "5", "num2": "3", "operation": "+"})
-        assert b"8.0" in response.data
